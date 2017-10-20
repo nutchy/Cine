d04be3bfa694f77de48a2c202c44f4e8c827aac8
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-10-21 02:13:25 +0700

    Login with Facebook

--- Cine/app/src/main/java/me/nutchy/cine/MainActivity.java
+++ Cine/app/src/main/java/me/nutchy/cine/MainActivity.java
@@ -1,13 +1,46 @@
 package me.nutchy.cine;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import me.nutchy.cine.fragment.LoginFragment;
+
+
+public class MainActivity extends FragmentActivity {
+
+
+    private LoginFragment loginFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            loginFragment = new LoginFragment();
+
+
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, new LoginFragment())
+                    .commit();
+        } else {
+
+            // Or set the fragment from restored state info
+            loginFragment = (LoginFragment) fragmentManager.findFragmentById(R.id.fragment_container);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginFragment = (LoginFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        loginFragment.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ Cine/app/src/main/java/me/nutchy/cine/fragment/LoginFragment.java
@@ -0,0 +1,76 @@
+package me.nutchy.cine.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import me.nutchy.cine.R;
+
+public class LoginFragment extends Fragment {
+
+    // Tag for debug
+    private static String TAG = LoginFragment.class.getSimpleName();
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+    TextView tvStatus;
+
+    public static LoginFragment newInstance(){
+        return new LoginFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootview = inflater.inflate(R.layout.fragment_login, container, false);
+
+        tvStatus = rootview.findViewById(R.id.login_status);
+        loginButton = rootview.findViewById(R.id.login_button);
+
+        loginButton.setReadPermissions("email");
+
+        // If using in a fragment
+        loginButton.setFragment(this);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        // Callback registration
+        // Use Fragment don't need to Register Callback Manager on Login Manager
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                tvStatus.setText("AccessToken : "+loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                tvStatus.setText("Status : Cancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+        return rootview;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
