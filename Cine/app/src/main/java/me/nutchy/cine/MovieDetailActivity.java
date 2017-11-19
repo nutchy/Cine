89d994c5e6da705d73e1f59464a236f3fd88d637
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-11-19 16:29:51 +0700

    create layout for display movie detail.

--- Cine/app/src/main/java/me/nutchy/cine/MovieDetailActivity.java
+++ Cine/app/src/main/java/me/nutchy/cine/MovieDetailActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.support.v7.widget.Toolbar;
+
+import com.bumptech.glide.Glide;
 
 import butterknife.ButterKnife;
 import me.nutchy.cine.Model.Movie;
@@ -20,6 +24,16 @@ public class MovieDetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Movie movie = intent.getParcelableExtra("movie");
         Log.e(TAG_TITLE, movie.getTitle());
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        ImageView iV_poster = (ImageView) findViewById(R.id.iV_poster);
+        Glide.with(this).load(movie.BASE_URL_POSTER+movie.getPoster_path())
+                .into(iV_poster);
 
     }
 }
