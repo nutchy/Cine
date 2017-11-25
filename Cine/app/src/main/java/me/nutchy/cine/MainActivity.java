ee1479c78bc0bead19e3b2322a51527bf7cc6f43
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-11-25 21:17:44 +0700

    Update Nav Menu

--- Cine/app/src/main/java/me/nutchy/cine/MainActivity.java
+++ Cine/app/src/main/java/me/nutchy/cine/MainActivity.java
@@ -207,20 +207,8 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        if (id == R.id.logout) {
             userLogout();
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
