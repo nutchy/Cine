70e57e607d3fdbd0d3e9519bb01ff999aacc40c1
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-12-06 04:52:35 +0700

    Espresso Testing : Rating movie

--- Cine/app/src/androidTest/java/me/nutchy/cine/MainActivityTest.java
+++ Cine/app/src/androidTest/java/me/nutchy/cine/MainActivityTest.java
@@ -1,10 +1,15 @@
 package me.nutchy.cine;
 
-
-import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.widget.RatingBar;
+
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +21,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -69,6 +75,53 @@ public class MainActivityTest {
         onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
         onView(withText(R.string.app_name)).check(matches(withParent(withId(R.id.toolbar))));
     }
+
+    @Test
+    public void voteIt_5_Star(){
+        onView(withRecyclerView(R.id.rc_popular_movies)
+                .atPositionOnView(0, R.id.tv_movieName))
+                .check(matches(withText("It")));
+        onView(withRecyclerView(R.id.rc_popular_movies)
+                .atPositionOnView(0, R.id.iV_poster)).perform(click());
+        onView(withId(R.id.youStar)).check(matches(isDisplayed())).perform(click());
+        onView(withId(R.id.rating_bar)).perform(setProgress(5));
+        onView(withId(R.id.btn_rating)).perform(click());
+        onView(withId(R.id.tv_your_rate)).check(matches(withText("5/10")));
+    }
+
+    @Test
+    public void voteThor_7_Star(){
+        onView(withRecyclerView(R.id.rc_popular_movies)
+                .atPositionOnView(1, R.id.tv_movieName))
+                .check(matches(withText("Thor: Ragnarok")));
+        onView(withRecyclerView(R.id.rc_popular_movies)
+                .atPositionOnView(1, R.id.iV_poster)).perform(click());
+        onView(withId(R.id.youStar)).check(matches(isDisplayed())).perform(click());
+        onView(withId(R.id.rating_bar)).perform(setProgress(10));
+        onView(withId(R.id.btn_rating)).perform(click());
+        onView(withId(R.id.tv_your_rate)).check(matches(withText("10/10")));
+    }
+
+    public static ViewAction setProgress(final int progress) {
+        return new ViewAction() {
+            @Override
+            public void perform(UiController uiController, View view) {
+                ((RatingBar) view).setRating(progress);
+            }
+
+            @Override
+            public String getDescription() {
+                return "Set a progress";
+            }
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isAssignableFrom(RatingBar.class);
+            }
+        };
+    }
 }
+
+
 
 
